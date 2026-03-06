d4d8c095ebee141b50c263bc5ca7e5b9700e1d8f
david schummer <david.schummer@ogs.de>
2026-03-06 08:46:26 +0100

    feat(guthries-equipment): improve component register api

--- src/options.ts
+++ src/options.ts
@@ -2,38 +2,39 @@ import  {type ElementType} from "react";
 import {DEFAULT_COMPONENTS, FLOW_CONTROL_COMPONENTS, HTML_COMPONENTS} from "./constants";
 
 type EquipmentOptions = {
-  withIntrinsics?: boolean,
-  withFlowControl?: boolean,
+  withIntrinsics?: boolean;
+  withFlowControl?: boolean;
   elementMapping?: (value: string) => string };
 
 const DEFAULT_OPTIONS: EquipmentOptions = {
   withIntrinsics: true,
   withFlowControl: true
 }
-/*TODO: das gedefaulte gefällt mir noch nicht so*/
 
 function withGuthriesEquipment({components, options}: {components?: Record<string, ElementType>, options?: EquipmentOptions}): Record<string, ElementType> {
-  const _components = components ?? DEFAULT_COMPONENTS;
-  const _options = options ?? DEFAULT_OPTIONS
+  if (!components && !options)
+    return {...DEFAULT_COMPONENTS};
 
-  _options.withIntrinsics = _options.withIntrinsics ?? true;
-  _options.withFlowControl = _options.withFlowControl ?? true;
+  const userComponents: Record<string, ElementType> = components ?? {};
+  const userOptions = {...DEFAULT_OPTIONS, ...options}
 
-  let componentSet = {};
+  let componentSet: Record<string, ElementType> = {};
 
-  if (_options.withIntrinsics )
-    componentSet = HTML_COMPONENTS;
+  if (userOptions.withIntrinsics)
+    componentSet = {...HTML_COMPONENTS};
 
-  if(_options.withFlowControl)
+  if(userOptions.withFlowControl)
     componentSet = {...componentSet, ...FLOW_CONTROL_COMPONENTS}
 
-  componentSet = {..._components, ...componentSet};
+  componentSet = {...componentSet, ...userComponents};
 
-  if (!_options.elementMapping)
+  const entries = Object.entries(componentSet);
+
+  if (!userOptions.elementMapping)
     return componentSet;
 
   return Object.fromEntries(
-    Object.entries(_components).map(([key, value])=> [_options.elementMapping!(key), value])
+    entries.map(([key, value])=> [userOptions.elementMapping!(key), value])
   );
 }
 
